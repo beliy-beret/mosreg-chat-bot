e4a0f55d50802b91716bcb8db76308a519aa23b6
Stanislav Skorobogatot <skoriy12990@gmail.com>
2025-03-24 12:29:04 +0300

    comment change dialog title feature

--- src/components/Dialog/History/Title/index.tsx
+++ src/components/Dialog/History/Title/index.tsx
@@ -1,5 +1,3 @@
-import { Pen } from 'assets/icons';
-import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import style from './style.module.scss';
 
 type Props = {
@@ -7,50 +5,54 @@ type Props = {
 };
 
 export const Title = ({ themeName }: Props) => {
-  const [isEdit, setIsEdit] = useState(false);
-  const [inputValue, setInputValue] = useState(themeName);
+  // const [isEdit, setIsEdit] = useState(false);
+  // const [inputValue, setInputValue] = useState(themeName);
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.currentTarget.value);
-  };
+  // const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setInputValue(event.currentTarget.value);
+  // };
 
-  const sendNewTitle = () => {
-    setIsEdit(false);
-  };
+  // const sendNewTitle = () => {
+  //   setIsEdit(false);
+  // };
 
-  const onEnterPress = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      sendNewTitle();
-    }
-  };
+  // const onEnterPress = (event: KeyboardEvent) => {
+  //   if (event.key === 'Enter') {
+  //     sendNewTitle();
+  //   }
+  // };
 
   return (
     <div className={style.wrapper}>
-      {!isEdit && (
-        <div className={style.title}>
-          <h1>{themeName}</h1>
-          <button className="btn black" onClick={() => setIsEdit(true)}>
-            <Pen />
-          </button>
-        </div>
-      )}
+      <div className={style.title}>
+        <h1>{themeName}</h1>
+      </div>
 
-      {isEdit && (
-        <div className={style.titleForm}>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={onChange}
-            onBlur={sendNewTitle}
-            onKeyDown={onEnterPress}
-            autoFocus
-          />
+      {/*{!isEdit && (*/}
+      {/*  <div className={style.title}>*/}
+      {/*    <h1>{themeName}</h1>*/}
+      {/*    <button className="btn black" onClick={() => setIsEdit(true)}>*/}
+      {/*      <Pen />*/}
+      {/*    </button>*/}
+      {/*  </div>*/}
+      {/*)}*/}
 
-          <button className="btn black" onClick={() => setIsEdit(true)}>
-            <Pen />
-          </button>
-        </div>
-      )}
+      {/*{isEdit && (*/}
+      {/*  <div className={style.titleForm}>*/}
+      {/*    <input*/}
+      {/*      type="text"*/}
+      {/*      value={inputValue}*/}
+      {/*      onChange={onChange}*/}
+      {/*      onBlur={sendNewTitle}*/}
+      {/*      onKeyDown={onEnterPress}*/}
+      {/*      autoFocus*/}
+      {/*    />*/}
+
+      {/*    <button className="btn black" onClick={() => setIsEdit(true)}>*/}
+      {/*      <Pen />*/}
+      {/*    </button>*/}
+      {/*  </div>*/}
+      {/*)}*/}
     </div>
   );
 };
