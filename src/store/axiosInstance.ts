4bba6741ab637f34cee407f9b52ae3477b878c3e
Stanislav Skorobogatot <skoriy12990@gmail.com>
2025-03-25 10:59:02 +0300

    add .env to config

--- src/store/axiosInstance.ts
+++ src/store/axiosInstance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/';
+
 export const instance = axios.create({
-  baseURL: 'http://dev.invest.mosreg.ru:10012',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
